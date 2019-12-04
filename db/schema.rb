e99cedce1bd6c0b5a4d9a6bd70c742ee24e4d32e
Philippe chambon <joan_krugg@live.fr>
2019-12-04 19:26:13 +0100

    add photo uploader and column pho to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_172344) do
+ActiveRecord::Schema.define(version: 2019_12_04_182257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_12_04_172344) do
     t.string "nickname"
     t.integer "age"
     t.string "city"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
