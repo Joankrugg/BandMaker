97df948c47b89afc8bf6953055bce7ba7f818afc
Philippe chambon <joan_krugg@live.fr>
2020-01-18 15:59:28 +0100

    home page design

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'applyings/create'
   get 'applyings/destroy'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'publications#index'
   resources :users, only: [:index, :show, :edit, :update, :destroy] do
     member do
       post :follow
